4c5eb976d4d4bdd00f7a14118697159452ec7648
pratik kad <pratikkad22@gmail.com>
2025-02-27 19:45:00 +0530

    restaurant owner middleware

--- middleware/restaurantOwner.middleware.js
+++ middleware/restaurantOwner.middleware.js
@@ -1,1 +1,20 @@
 //restaurant Owner middleware
+require('dotenv').config();
+async function restaurantOwnerMiddleware(req,res,next) {
+    try {
+        const token=req.headers['token'];
+        if(token){
+            const response=jwt.verify(token, process.env.JWT_SECRET_RESTAURANTOWNER);
+            req.ownerId=response.id;
+            next();
+        }
+    } catch (error) {
+        res.status(401).json({
+            message:"Unauthorized!",
+            error:error
+        })
+    }
+    
+}
+
+module.exports = restaurantOwnerMiddleware;
